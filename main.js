62ac3516a4777d259fe358b8f8bee3d201244511
juba1508 <jule.bauer96@web.de>
2023-05-25 16:31:18 +0200

    MEtada

--- main.js
+++ main.js
@@ -41,6 +41,14 @@ async function showForecast(url, latlng) {
 
     let markup = `
         <h4>Aktuelles Wetter für ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)}</h4>
+        <table>
+            <tr><td>Air pressure at sea leven (hPa)</td><td>${current.air_pressure_at_sea_level}</td></tr>
+            <tr><td>Air temperature (Celsius)</td><td>${current.air_temperature}</td></tr>
+            <tr><td>Cloud area fraction (%)</td><td>${current.cloud_area_fraction}</td></tr>
+            <tr><td>Relative humidity</td><td>${current.relative_humidity}</td></tr>
+            <tr><td>Wind from direction (°C)</td><td>${current.wind_from_direction}</td></tr>
+            <tr><td>Wind speed</td><td>>${current.wind_speed}</td></tr>
+        </table>
     `;
 
     L.popup().setLatLng(latlng).setContent(markup).openOn(map);
